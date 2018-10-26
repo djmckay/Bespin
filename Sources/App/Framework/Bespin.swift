b7c1bfc79583f52647f5b7c5921f1c66d95eb6b5
DJ <djmckay@mac.com>
2018-10-26 09:52:25 -0400

    Add missing configs for vapor cloud

--- Sources/App/Framework/Bespin.swift
+++ Sources/App/Framework/Bespin.swift
@@ -13,10 +13,10 @@ struct Bespin {
     static fileprivate let DatabaseUsername: String = Environment.get("DATABASE_USER") ?? "bespin"
     static fileprivate let DatabasePassword: String = Environment.get("DATABASE_PASSWORD") ?? "password"
     
-    static let BespinConfig = MySQLDatabaseConfig(hostname: "localhost", port: 3306, username: DatabaseUsername, password: DatabasePassword, database: "bespin")
+    static let BespinConfig = MySQLDatabaseConfig(hostname: Environment.get("DATABASE_HOSTNAME") ?? "localhost", port: 3306, username: DatabaseUsername, password: DatabasePassword, database: Environment.get("DATABASE_DB") ?? "bespin")
     static let Bespin = MySQLDatabase(config: BespinConfig)
     
-    static let BespinConfigTest = MySQLDatabaseConfig(hostname: "localhost", port: 3307, username: DatabaseUsername, password: DatabasePassword, database: "bespin-test")
+    static let BespinConfigTest = MySQLDatabaseConfig(hostname: Environment.get("DATABASE_HOSTNAME") ?? "localhost", port: 3307, username: DatabaseUsername, password: DatabasePassword, database: Environment.get("DATABASE_DB") ?? "bespin-test")
     static let BespinTest = MySQLDatabase(config: BespinConfigTest)
 }
 
