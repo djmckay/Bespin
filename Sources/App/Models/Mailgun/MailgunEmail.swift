451250ba4fc6bdefc6fda9d0e524fc7ccba1cedc
DJ <djmckay@mac.com>
2019-01-28 11:24:26 -0500

    attachment File type

--- Sources/App/Models/Mailgun/MailgunEmail.swift
+++ Sources/App/Models/Mailgun/MailgunEmail.swift
@@ -28,8 +28,9 @@ public protocol MailgunEmailType: Content {
     
     /// An array of objects in which you can specify any attachments you want to include.
     //var attachments: [EmailAttachment]? { get set }
-    var attachment1: EmailAttachment? { get set }
-    var attachment2: EmailAttachment? { get set }
+    var attachment:  [File]? { get set }
+//    var attachment1: EmailAttachment? { get set }
+//    var attachment2: EmailAttachment? { get set }
     var recipientVariables: String? { get set }
     var deliveryTime: String? { get set }
     var testmode: Bool? { get set }
@@ -59,9 +60,9 @@ public struct MailgunEmail: MailgunEmailType {
     
     /// An array of objects in which you can specify any attachments you want to include.
     //public var attachments: [EmailAttachment]?
-    public var attachment1: EmailAttachment?
-    public var attachment2: EmailAttachment?
-    
+//    public var attachment1: EmailAttachment?
+//    public var attachment2: EmailAttachment?
+    public var attachment: [File]?
     public typealias RecipientVariables = [String: [String: String]]
     
     public var recipientVariables: String?
@@ -85,11 +86,18 @@ public struct MailgunEmail: MailgunEmailType {
         self.text = text
         self.html = html
         self.subject = subject
-        //self.attachments = attachments
-        self.attachment1 = attachments?.first
-        if attachments?.count == 2 {
-            self.attachment2 = attachments![1]
+        if let emailAttachments = attachments {
+            self.attachment = []
+            for emailAttachment in emailAttachments {
+                let anAttachment = File(data: emailAttachment.data, filename: emailAttachment.filename)
+                self.attachment?.append(anAttachment)
+            }
         }
+        //self.attachments = attachments
+//        self.attachment1 = attachments?.first
+//        if attachments?.count == 2 {
+//            self.attachment2 = attachments![1]
+//        }
         self.testmode = testmode
         if let deliveryTime = deliveryTime {
             let formatter = DateFormatter()
@@ -133,9 +141,10 @@ public struct MailgunEmail: MailgunEmailType {
         case text
         case html
         case subject
+        case attachment
         //case attachments = "attachment"
-        case attachment1 = "attachment[0]"
-        case attachment2 = "attachment[1]"
+//        case attachment1 = "attachment[0]"
+//        case attachment2 = "attachment[1]"
         case recipientVariables = "recipient-variables"
         case deliveryTime = "o:deliverytime"
         case testmode = "o:testmode"
@@ -180,8 +189,9 @@ public struct MailgunEmailPlus<Element: Codable>: MailgunEmailType {
     
     /// An array of objects in which you can specify any attachments you want to include.
     //public var attachments: [EmailAttachment]?
-    public var attachment1: EmailAttachment?
-    public var attachment2: EmailAttachment?
+    public var attachment: [File]?
+//    public var attachment1: EmailAttachment?
+//    public var attachment2: EmailAttachment?
     //var data: Element
     public var recipientVariables: String?
     public var deliveryTime: String?
@@ -211,11 +221,18 @@ public struct MailgunEmailPlus<Element: Codable>: MailgunEmailType {
         self.html = html
         self.subject = subject
         //self.attachments = attachments
-        self.testmode = testmode
-        self.attachment1 = attachments?.first
-        if attachments?.count == 2 {
-            self.attachment2 = attachments![1]
+        if let emailAttachments = attachments {
+            self.attachment = []
+            for emailAttachment in emailAttachments {
+                let anAttachment = File(data: emailAttachment.data, filename: emailAttachment.filename)
+                self.attachment?.append(anAttachment)
+            }
         }
+        self.testmode = testmode
+//        self.attachment1 = attachments?.first
+//        if attachments?.count == 2 {
+//            self.attachment2 = attachments![1]
+//        }
         if let deliveryTime = deliveryTime {
             let formatter = DateFormatter()
             formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss Z"
@@ -240,11 +257,52 @@ public struct MailgunEmailPlus<Element: Codable>: MailgunEmailType {
         case html
         case subject
         //case attachments = "attachment"
-        case attachment1 = "attachment[0]"
-        case attachment2 = "attachment[1]"
+        case attachment
+//        case attachment1 = "attachment[0]"
+//        case attachment2 = "attachment[1]"
         case recipientVariables = "recipient-variables"
         case deliveryTime = "o:deliverytime"
         case testmode = "o:testmode"
     }
 }
 
+
+public struct MailgunAttachment: Content {
+    
+
+    
+    /// The Base64 encoded content of the attachment.
+    public var content: String?
+
+    /// The mime type of the content you are attaching. For example, “text/plain” or “text/html”.
+    public var type: String?
+
+    /// The filename of the attachment.
+    public var filename: String?
+
+    /// The content-disposition of the attachment specifying how you would like the attachment to be displayed.
+    public var disposition: String?
+
+    /// The content id for the attachment. This is used when the disposition is set to “inline” and the attachment is an image, allowing the file to be displayed within the body of your email.
+    public var contentId: String?
+
+    public init(content: String? = nil,
+                type: String? = nil,
+                filename: String? = nil,
+                disposition: String? = nil,
+                contentId: String? = nil) {
+        self.content = content
+        self.type = type
+        self.filename = filename
+        self.disposition = disposition
+        self.contentId = contentId
+    }
+
+    public enum CodingKeys: String, CodingKey {
+        case content
+        case type
+        case filename
+        case disposition
+        case contentId = "content_id"
+    }
+}
