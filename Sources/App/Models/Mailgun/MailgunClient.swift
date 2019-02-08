38168f2fcd82d887144524e28ad2344e1ebd1f40
DJ <djmckay@mac.com>
2019-02-07 20:23:35 -0500

    Use x-mailgun-native-send

--- Sources/App/Models/Mailgun/MailgunClient.swift
+++ Sources/App/Models/Mailgun/MailgunClient.swift
@@ -29,6 +29,7 @@ public final class MailgunClient: Service {
         let authKeyEncoded = try encode(apiKey: apiKey)
             var headers: HTTPHeaders = [:]
             headers.add(name: HTTPHeaderName.authorization, value: "Basic \(authKeyEncoded)")
+            headers.add(name: "x-mailgun-native-send", value: "true")
             let encoder = JSONEncoder()
             encoder.dateEncodingStrategy = .secondsSince1970
             
@@ -49,6 +50,7 @@ public final class MailgunClient: Service {
             
             var headers: HTTPHeaders = [:]
             headers.add(name: HTTPHeaderName.authorization, value: "Basic \(authKeyEncoded)")
+            headers.add(name: "x-mailgun-native-send", value: "true")
             let encoder = JSONEncoder()
             encoder.dateEncodingStrategy = .secondsSince1970
             
