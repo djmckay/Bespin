484f45cd64408e77891e5bc04ad3d1d6af350d67
DJ <djmckay@mac.com>
2019-07-04 18:52:34 -0400

    Fix s3 region

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -70,7 +70,7 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
         bucket: Bespin.Storage_Bucket,
         accessKey: Bespin.Storage_Key,
         secretKey: Bespin.Storage_Secret,
-        region: .usEast1,
+        region: .usEast2,
         pathTemplate: "/bespin/#folder/#file"
         
     )
