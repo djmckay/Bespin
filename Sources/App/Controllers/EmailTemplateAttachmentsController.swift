9d597816c3a7fcdc7b826356b0e641dfa8706bfd
DJ <djmckay@mac.com>
2019-07-04 17:17:35 -0400

    Fix attachment routes.

--- Sources/App/Controllers/EmailTemplateAttachmentsController.swift
+++ Sources/App/Controllers/EmailTemplateAttachmentsController.swift
@@ -56,7 +56,7 @@ struct EmailTemplateAttachmentsController: BespinController {
     typealias Public = EmailTemplateAttachment
     
     func boot(router: Router) throws {
-        let route = router.grouped(BespinApi.path, Token.parameter, EmailTemplate.parameter, EmailTemplateAttachmentsController.path)
+        let route = router.grouped(BespinApi.path, Token.parameter, EmailTemplateController.path, EmailTemplate.parameter, EmailTemplateAttachmentsController.path)
         let protectedRoutes = route.grouped(JWTMiddleWareProvider())
         protectedRoutes.get(T.parameter, use: getHandler)
         protectedRoutes.get(use: getAllHandler)
