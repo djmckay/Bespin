f19b3c6a1a08323bb35a5cd6fd703e860175ba65
DJ <djmckay@mac.com>
2019-07-04 17:06:20 -0400

    Add Attachment routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,4 +6,5 @@ public func routes(_ router: Router) throws {
     try router.register(collection: UsersController())
     try router.register(collection: TokensController())
     try router.register(collection: MessagesController())
+    try router.register(collection: EmailTemplateAttachmentsController())
 }
